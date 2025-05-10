b4dedc5df14c8e1787367e57f9db4b693e83cf69
Vaishnavi <82608700+Vaitae@users.noreply.github.com>
2025-05-10 22:03:07 +0530

    Time: 0 ms (100%), Space: 12.7 MB (19.93%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isValid(self, s):
+        stack=[]
+        parenthesis={")":"(","}":"{","]":"["}
+
+        for c in s:
+            if c in parenthesis:
+                if stack and stack[-1]==parenthesis[c]:
+                    stack.pop() 
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return stack==[]
+
+        
